8ddaf9c8902ccedeedc384d300583c96040b8a22
Felix Wang <yanweiw@middlebury.edu>
2017-10-17 21:37:55 -0500

    add spacing to 40 mm

--- src/setup.cpp
+++ src/setup.cpp
@@ -1,7 +1,7 @@
-#define ROBOT_COUNT 1024
+#define ROBOT_COUNT 32*32
 
-#define ARENA_WIDTH 1024
-#define ARENA_HEIGHT 1024
+#define ARENA_WIDTH 32*32 + 33*40
+#define ARENA_HEIGHT 32*32 + 33*40
 
 #define SIMULATION_TIME 180 //in seconds
 
@@ -9,14 +9,15 @@ void setup_positions(float robot_pos[ROBOT_COUNT][3])
 {
 	// I modified original codes to setup 32*32 immobile kilobots
 	int columns = 32;
-	int horizontal_separation = 32;
-	int vertical_separation = 32;
+	// 40 mm spacing between robots
+	int horizontal_separation = 32 + 40;
+	int vertical_separation = 32 + 40;
 	for (int i = 0;i < ROBOT_COUNT;i++)
 	{
 		int c = i % columns;
 		int r = i / columns;
-		robot_pos[i][0] = c * horizontal_separation + 16; //x
-		robot_pos[i][1] = r * vertical_separation + 16;   //y
+		robot_pos[i][0] = c * horizontal_separation + 16 + 40; //x
+		robot_pos[i][1] = r * vertical_separation + 16 + 30;   //y
 		robot_pos[i][2] = 0; //theta
 	}
 }
