0ab3324e62286c5c8869cf7ccf1b077729cbe82b
Felix Wang <yanweiw@middlebury.edu>
2017-10-22 19:11:13 -0500

    my gradient implementation, could have bug

--- src/kilobot.cpp
+++ src/kilobot.cpp
@@ -7,6 +7,7 @@
 #define ARENA_HEIGHT 32*32 + 33*ROBOT_SPACING
 #define MAX_HOPCOUNT 254 // the largest even num < 255 to initialize dark screen
 #define MIN(a, b) (a < b ? a : b)
+#define LEARNING_RATE 0.0008
 
 class mykilobot : public kilobot
 {
@@ -28,23 +29,46 @@ class mykilobot : public kilobot
 	};
 
 	mydata hopcnt; // to store information of hopcounts from the two seeds
-	mydata estpos; // estimated position
+	mydata estpos; // estimated position in coords
 	mydata color;	 // two color to display for debugging
-	mydata seed1;  // position of seed 1
-	mydata seed2;  // position of seed 2
+	mydata seed1;  // position of seed 1 in rows and cols
+	mydata seed2;  // position of seed 2 in rows and cols
+
+	mydata calcRowColPos(int x, int y) {
+		mydata rowcol;
+		rowcol.data1 = x / (radius*2 + ROBOT_SPACING);
+		rowcol.data2 = y / (radius*2 + ROBOT_SPACING);
+		return rowcol;
+	};
+
+	double calcCoord(int RowColPos) {
+		double coord = (RowColPos + 1) * (ROBOT_SPACING + radius*2) - radius;
+		return coord;
+	}
+
+	double calcError(int currX, int currY) {
+		double hopdist1 = (hopcnt.data1-1) * comm_range;
+		double hopdist2 = (hopcnt.data2-1) * comm_range;
+		double seed1x = calcCoord(seed1.data1);
+		double seed1y = calcCoord(seed1.data2);
+		double seed2x = calcCoord(seed2.data1);
+		double seed2y = calcCoord(seed2.data2);
+		double distToSeed1 = distance(currX, currY, ROBOT_SPACING+16, ROBOT_SPACING+16);
+		double distToSeed2 = distance(currX, currY, (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16);
+		double error = pow(distance(hopdist1, hopdist2, distToSeed1, distToSeed2), 2);
+		return error;
+	}
 
 	//main loop
 	void loop()
 	{
-		//update message
-		if (id == 1) {
+		//update message, we only know pos of seeds whose id are 1 or 2
+		if (id == 0) {
 			hopcnt.data1 = 1;
-			seed1.data1 = pos[0];
-			seed1.data2 = pos[1];
-		} else if (id == 2) {
+			seed1 = calcRowColPos((int)pos[0], (int)pos[1]);
+		} else if (id == 31) {
 			hopcnt.data2 = 1;
-			seed2.data1 = pos[0];
-			seed2.data2 = pos[1];
+			seed2 = calcRowColPos((int)pos[0], (int)pos[1]);
 		}
 		out_message.type = NORMAL;
 		out_message.data[0] = MIN(hopcnt.data1 + 1, MAX_HOPCOUNT);
@@ -69,31 +93,67 @@ class mykilobot : public kilobot
 			}
 		}
 		//gradient decent
-		// else {
-		// 	double hopdist_x = hopcnt.data1 * comm_range;
-		// 	double hopdist_y = hopcnt.data2 * comm_range;
-		// 	double distToSeed1 = distance(estpos.data1, estpos.data2, 0, hopdist_y);
-		//
-		//
-		//
-		//
-		// 	if (estpos.data1 % 2 == 1) {
-		// 		color.data1 = 3;
-		// 	} else {
-		// 		color.data1 = 0;
-		// 	}
-		// 	if (estpos.data2 % 2 == 1) {
-		// 		color.data2 = 3;
-		// 	} else {
-		// 		color.data2 = 0;
-		// 	}
-		// }
+		else {
+			int currX = estpos.data1;
+			int currY = estpos.data2;
+			double e0 = calcError(currX, currY);
+			double e1 = calcError(currX-1, currY);
+			double e2 = calcError(currX+1, currY);
+			double e3 = calcError(currX, currY-1);
+			double e4 = calcError(currX, currY+1);
+			if (e0 > e1) {
+				if (e0 > e2 && e0-e2 > e0-e1) {
+					estpos.data1 = currX + (int)(LEARNING_RATE*(e0-e2));
+				} else {
+					estpos.data1 = currX - (int)(LEARNING_RATE*(e0-e1));
+				}
+			} else if (e0 > e2) {
+				estpos.data1 = currX + (int)(LEARNING_RATE*(e0-e2));
+			}
+			if (e0 > e3) {
+				if (e0 > e4 && e0-e4 > e0-e3) {
+					estpos.data2 = currY + (int)(LEARNING_RATE*(e0-e4));
+				} else {
+					estpos.data2 = currY - (int)(LEARNING_RATE*(e0-e3));
+				}
+			} else if (e0 > e4) {
+				estpos.data2 = currY + (int)(LEARNING_RATE*(e0-e4));
+			}
+
+			if (iteration == 2500 && (id == 0 || id == 31 || id == 100 || id == 200 || id == 400 || id == 600 || id == 800 || id == 1000)) {
+				printf("id: %d; e0: %d; X: %d; Y: %d; x: %d; y: %d\n", id, (int)e0,(int)pos[0],(int)pos[1], estpos.data1, estpos.data2);
+				printf("truedist1: %d; calcdist1: %d; hopdist1: %d\n", (int)distance(pos[0], pos[1], ROBOT_SPACING+16, ROBOT_SPACING+16), (int)distance(currX, currY, ROBOT_SPACING+16, ROBOT_SPACING+16), (hopcnt.data1-1) * (int)comm_range);
+				printf("truedist2: %d; calcdist2: %d; hopdist2: %d\n\n", (int)distance(pos[0], pos[1], (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16), (int)distance(currX, currY, (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16), (hopcnt.data2-1)*(int)comm_range);
+			}
+
+			if (((estpos.data1+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+				color.data1 = 3;
+			} else {
+				color.data1 = 0;
+			}
+			if (((estpos.data2+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+				color.data2 = 3;
+			} else {
+				color.data2 = 0;
+			}
+
+			// if ((((int)pos[0]+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+			// 	color.data1 = 3;
+			// } else {
+			// 	color.data1 = 0;
+			// }
+			// if ((((int)pos[1]+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+			// 	color.data2 = 3;
+			// } else {
+			// 	color.data2 = 0;
+			// }
+		}
 
 		set_color(RGB(color.data1,0,color.data2));
 		// printf("%d\n", iteration);
 		iteration++;
-		printf("seed 1: %d, %d\n", seed1.data1, seed1.data2);
-		printf("seed 2: %d, %d\n", seed2.data1, seed2.data2);
+
+		// printf("pos: %d, %d\n", (int)pos[0], (int)pos[1]);
 
 		// now estimate coordinates after hopcounts stabilize
 		// if (iteration >= 1000)
@@ -127,6 +187,7 @@ class mykilobot : public kilobot
 		// float y = (float) rand() * (ARENA_HEIGHT-2*radius) / RAND_MAX + radius;
 		estpos.data1 = rand() % ARENA_WIDTH; // randomize initial x estimate
 		estpos.data2 = rand() % ARENA_HEIGHT; // randomize initial y estimate
+		// printf("intial x and y: %d, %d\n", estpos.data1, estpos.data2);
 
 		// out_message.type = NORMAL;
 		// out_message.data[0] = MIN(hopcnt.data1 + 1, MAX_HOPCOUNT); // to prevent negatives
