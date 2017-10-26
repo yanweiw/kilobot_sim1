59f97b16dedfea2f811009a908052d4f7b645a21
Felix Wang <yanweiw@middlebury.edu>
2017-10-25 21:20:04 -0500

    implemented smoothing single hopcnt for 8mm spacing

--- src/kilobot.cpp
+++ src/kilobot.cpp
@@ -2,18 +2,23 @@
 #include "kilolib.h"
 
 //my definitions for this assignment
-#define ROBOT_SPACING 55
+#define ROBOT_SPACING 8
 #define ARENA_WIDTH 32*32 + 33*ROBOT_SPACING
 #define ARENA_HEIGHT 32*32 + 33*ROBOT_SPACING
 #define MAX_HOPCOUNT 254 // the largest even num < 255 to initialize dark screen
 #define MIN(a, b) (a < b ? a : b)
 #define LEARNING_RATE 0.0008
+#define SMOOTHING 1
 
 class mykilobot : public kilobot
 {
 	unsigned char dist;
 	message_t out_message;
 	int rxed=0;
+	double avehop1 = 0;
+	double avehop2 = 0;
+	int count1;
+	int count2;
 
 	// set motion to stop
 	int motion=4;
@@ -76,10 +81,10 @@ class mykilobot : public kilobot
 		out_message.type = NORMAL;
 		out_message.data[0] = MIN(hopcnt.data1 + 1, MAX_HOPCOUNT);
 		out_message.data[1] = MIN(hopcnt.data2 + 1, MAX_HOPCOUNT);
-		out_message.data[2] = seed1.data1;	// broadcast seed 1 position
-		out_message.data[3] = seed1.data2;
-		out_message.data[4] = seed2.data1;	// broadcast seed 2 position
-		out_message.data[5] = seed2.data2;
+		// out_message.data[2] = seed1.data1;	// broadcast seed 1 position
+		// out_message.data[3] = seed1.data2;
+		// out_message.data[4] = seed2.data1;	// broadcast seed 2 position
+		// out_message.data[5] = seed2.data2;
 
 		//update color
 		if (iteration <= 1300) {
@@ -88,108 +93,137 @@ class mykilobot : public kilobot
 			} else {
 				color.data1 = 0;
 			}
-			if (hopcnt.data2 % 2 == 1) {
-				color.data2 = 2;
-			} else  {
-				color.data2 = 0;
-			}
-		}
-		//gradient decent
-		else {
-			int currX = estpos.data1;
-			int currY = estpos.data2;
-			double e0 = calcError(currX, currY);
-			double e1 = calcError(currX-1, currY);
-			double e2 = calcError(currX+1, currY);
-			double e3 = calcError(currX, currY-1);
-			double e4 = calcError(currX, currY+1);
-			if (e0 > e1) {
-				if (e0 > e2 && e0-e2 > e0-e1) {
-					estpos.data1 = currX + (int)(LEARNING_RATE*(e0-e2));
-				} else {
-					estpos.data1 = currX - (int)(LEARNING_RATE*(e0-e1));
-				}
-			} else if (e0 > e2) {
-				estpos.data1 = currX + (int)(LEARNING_RATE*(e0-e2));
-			}
-			if (e0 > e3) {
-				if (e0 > e4 && e0-e4 > e0-e3) {
-					estpos.data2 = currY + (int)(LEARNING_RATE*(e0-e4));
-				} else {
-					estpos.data2 = currY - (int)(LEARNING_RATE*(e0-e3));
-				}
-			} else if (e0 > e4) {
-				estpos.data2 = currY + (int)(LEARNING_RATE*(e0-e4));
-			}
-
-			if (iteration < 2500) {
-				if (((estpos.data1+16)/(32+ROBOT_SPACING)) % 2 == 1) {
-					color.data1 = 3;
-				} else {
-					color.data1 = 0;
-				}
-				if (((estpos.data2+16)/(32+ROBOT_SPACING)) % 2 == 1) {
-					color.data2 = 3;
-				} else {
-					color.data2 = 0;
-				}
-			}
-
-			if (iteration == 2500) {
-				color.data1=0;
-				color.data2=0;
-			}
-
-			if (iteration > 2500 && iteration < 2700) {
-				// printf("id: %d; e0: %d; X: %d; Y: %d; x: %d; y: %d\n", id, (int)e0,(int)pos[0],(int)pos[1], estpos.data1, estpos.data2);
-				// printf("truedist1: %d; calcdist1: %d; hopdist1: %d\n", (int)distance(pos[0], pos[1], ROBOT_SPACING+16, ROBOT_SPACING+16), (int)distance(currX, currY, ROBOT_SPACING+16, ROBOT_SPACING+16), (hopcnt.data1-1) * (int)comm_range);
-				// printf("truedist2: %d; calcdist2: %d; hopdist2: %d\n\n", (int)distance(pos[0], pos[1], (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16), (int)distance(currX, currY, (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16), (hopcnt.data2-1)*(int)comm_range);
-				// printf("robot %d has x: %d; and y: %d\n", id, estpos.data1, estpos.data2);
-				// int col_num = (estpos.data2 - 450) / 60 ;
-				// printf("col num: %d \n", col_num);
-				// if (col_num < 0) {
-				// 	col_num = 0;
-				// } else if (col_num > 15) {
-				// 	col_num = 15;
-				// }
-				// if (col_num / 12) {
-				// 	set_color(RGB((col_num % 4),0,0));
-				// } else if (col_num / 8) {
-				// 	set_color(RGB(0,(col_num % 4),0));
-				// } else if (col_num / 4) {
-				// 	set_color(RGB(0,0,(col_num % 4)));
-				// }  else {
-				// 	set_color(RGB((col_num % 4),0,0));
-				// }
-				int x = estpos.data1;
-				int y = estpos.data2;
-				if (x < 1550 || x > 2050 || ((0.95*x + 1.1*y)>2500&&(x+y)<2900) || amIpurple > 1) {
-					color.data1=2;
-					color.data2=3;
-					out_message.data[6] = 1;
-				} else {
-					out_message.data[6] = 0;
-				}
-				amIpurple=0;
-			}
-
-			// if ((((int)pos[0]+16)/(32+ROBOT_SPACING)) % 2 == 1) {
-			// 	color.data1 = 3;
-			// } else {
-			// 	color.data1 = 0;
-			// }
-			// if ((((int)pos[1]+16)/(32+ROBOT_SPACING)) % 2 == 1) {
-			// 	color.data2 = 3;
-			// } else {
+			// if (hopcnt.data2 % 2 == 1) {
+			// 	color.data2 = 2;
+			// } else  {
 			// 	color.data2 = 0;
 			// }
+			set_color(RGB(color.data1,0,color.data2));
 		}
-		set_color(RGB(color.data1,0,color.data2));
+		//gradient decent
+		// else {
+		// 	int currX = estpos.data1;
+		// 	int currY = estpos.data2;
+		// 	double e0 = calcError(currX, currY);
+		// 	double e1 = calcError(currX-1, currY);
+		// 	double e2 = calcError(currX+1, currY);
+		// 	double e3 = calcError(currX, currY-1);
+		// 	double e4 = calcError(currX, currY+1);
+		// 	if (e0 > e1) {
+		// 		if (e0 > e2 && e0-e2 > e0-e1) {
+		// 			estpos.data1 = currX + (int)(LEARNING_RATE*(e0-e2));
+		// 		} else {
+		// 			estpos.data1 = currX - (int)(LEARNING_RATE*(e0-e1));
+		// 		}
+		// 	} else if (e0 > e2) {
+		// 		estpos.data1 = currX + (int)(LEARNING_RATE*(e0-e2));
+		// 	}
+		// 	if (e0 > e3) {
+		// 		if (e0 > e4 && e0-e4 > e0-e3) {
+		// 			estpos.data2 = currY + (int)(LEARNING_RATE*(e0-e4));
+		// 		} else {
+		// 			estpos.data2 = currY - (int)(LEARNING_RATE*(e0-e3));
+		// 		}
+		// 	} else if (e0 > e4) {
+		// 		estpos.data2 = currY + (int)(LEARNING_RATE*(e0-e4));
+		// 	}
+		//
+		// 	if (iteration < 2500) {
+		// 		if (((estpos.data1+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+		// 			color.data1 = 3;
+		// 		} else {
+		// 			color.data1 = 0;
+		// 		}
+		// 		if (((estpos.data2+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+		// 			color.data2 = 3;
+		// 		} else {
+		// 			color.data2 = 0;
+		// 		}
+		// 	}
+		//
+		// 	if (iteration == 2500) {
+		// 		color.data1=0;
+		// 		color.data2=0;
+		// 	}
+		//
+		// 	if (iteration > 2500 && iteration < 2700) {
+		// 		// printf("id: %d; e0: %d; X: %d; Y: %d; x: %d; y: %d\n", id, (int)e0,(int)pos[0],(int)pos[1], estpos.data1, estpos.data2);
+		// 		// printf("truedist1: %d; calcdist1: %d; hopdist1: %d\n", (int)distance(pos[0], pos[1], ROBOT_SPACING+16, ROBOT_SPACING+16), (int)distance(currX, currY, ROBOT_SPACING+16, ROBOT_SPACING+16), (hopcnt.data1-1) * (int)comm_range);
+		// 		// printf("truedist2: %d; calcdist2: %d; hopdist2: %d\n\n", (int)distance(pos[0], pos[1], (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16), (int)distance(currX, currY, (ROBOT_SPACING+32)*32-16, ROBOT_SPACING+16), (hopcnt.data2-1)*(int)comm_range);
+		// 		// printf("robot %d has x: %d; and y: %d\n", id, estpos.data1, estpos.data2);
+		// 		// int col_num = (estpos.data2 - 450) / 60 ;
+		// 		// printf("col num: %d \n", col_num);
+		// 		// if (col_num < 0) {
+		// 		// 	col_num = 0;
+		// 		// } else if (col_num > 15) {
+		// 		// 	col_num = 15;
+		// 		// }
+		// 		// if (col_num / 12) {
+		// 		// 	set_color(RGB((col_num % 4),0,0));
+		// 		// } else if (col_num / 8) {
+		// 		// 	set_color(RGB(0,(col_num % 4),0));
+		// 		// } else if (col_num / 4) {
+		// 		// 	set_color(RGB(0,0,(col_num % 4)));
+		// 		// }  else {
+		// 		// 	set_color(RGB((col_num % 4),0,0));
+		// 		// }
+		// 		int x = estpos.data1;
+		// 		int y = estpos.data2;
+		// 		if (x < 1550 || x > 2050 || ((0.95*x + 1.1*y)>2500&&(x+y)<2900) || amIpurple > 1) {
+		// 			color.data1=2;
+		// 			color.data2=3;
+		// 			out_message.data[6] = 1;
+		// 		} else {
+		// 			out_message.data[6] = 0;
+		// 		}
+		// 		amIpurple=0;
+		// 	}
+		//
+		// 	// if ((((int)pos[0]+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+		// 	// 	color.data1 = 3;
+		// 	// } else {
+		// 	// 	color.data1 = 0;
+		// 	// }
+		// 	// if ((((int)pos[1]+16)/(32+ROBOT_SPACING)) % 2 == 1) {
+		// 	// 	color.data2 = 3;
+		// 	// } else {
+		// 	// 	color.data2 = 0;
+		// 	// }
+		// }
+
+		if (SMOOTHING) {
+			if (iteration > 1300) {
+				int r=0, g=0, b=0, val = 0;
+				double hop1 = (avehop1+hopcnt.data1)/(count1+1);
+				// printf("hop1: %f\n", hop1 - (int)hop1);
+				if ((hop1 - (int)hop1) <= 0.33) {
+					val = 1;
+				} else if ((hop1 - (int)hop1) <= 0.66) {
+					val = 2;
+				} else {
+					val = 3;
+				}
+				if ((int)hop1 % 3 == 0) {
+					r = val;
+				} else if ((int)hop1 % 3 == 1) {
+					g = val;
+				} else {
+					b = val;
+				}
+				set_color(RGB(r,g,b));
+				out_message.data[7] = hopcnt.data1;
+				out_message.data[8] = hopcnt.data2;
+			}
+		}
+
 
 		// printf("%d\n", iteration);
 		iteration++;
 		out_message.crc = message_crc(&out_message);
-
+		// count1=0;
+		// avehop1=0;
+		// count2=0;
+		// avehop2=0;
 	}
 
 	//executed once at start
@@ -199,6 +233,10 @@ class mykilobot : public kilobot
 		hopcnt.data1 = MAX_HOPCOUNT;
 		hopcnt.data2 = MAX_HOPCOUNT;
 		amIpurple=0;
+		avehop1=0;
+		count1 =0;
+		avehop2=0;
+		count2 =0;
 		// float x = (float) rand() * (ARENA_WIDTH-2*radius) / RAND_MAX + radius;
 		// float y = (float) rand() * (ARENA_HEIGHT-2*radius) / RAND_MAX + radius;
 		estpos.data1 = rand() % ARENA_WIDTH; // randomize initial x estimate
@@ -243,8 +281,14 @@ class mykilobot : public kilobot
 		// seed1.data2 = message->data[3];
 		// seed2.data1 = message->data[4];
 		// seed2.data2 = message->data[5];
-		if (iteration > 2600 && message->data[6]) {
-			amIpurple++;
+		// if (iteration > 2600 && message->data[6]) {
+		// 	amIpurple++;
+		// }
+		if (iteration > 1300) {
+			avehop1 += message->data[7];
+			count1 += 1;
+			avehop2 += message->data[8];
+			count2 += 1;
 		}
 		rxed=1;
 	}
